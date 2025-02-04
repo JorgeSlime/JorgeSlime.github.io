52542e423b7a58e9b2e4814d0cbd8100a25940e9
JorgeSlime <jt603691@gmail.com>
2025-02-04 00:55:33 -0400

    fix deploy 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 export default defineConfig({
-    site: 'https://jorgeslime.github.io',
-    //base: '/Slime-s-Library/',
+    site: 'https://jorgeslime.github.io/Slime-s-Library/',
+    // base: '/Slime-s-Library/',
     output: "static",  
     markdown: {
         shikiConfig: {
