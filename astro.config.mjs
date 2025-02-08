d248c8c13275acaed50d3584d4b5a5bb2e672d29
JorgeSlime <jt603691@gmail.com>
2025-02-07 23:34:16 -0400

    update home page and general eschematics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx'; 
 import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
     site: 'https://jorgeslime.github.io',
-    // base: '/Slime-s-Library/',
     output: 'static',
     markdown: {
         shikiConfig: {
             theme: 'dracula',
         },
     },
-    integrations: [mdx(), tailwind()],
+    integrations: [
+        mdx({
+            syntaxHighlight: 'shiki',
+            shikiConfig: { theme: 'dracula' },
+            // remarkPlugins: [import('remark-math')],
+            // rehypePlugins: [import('rehype-katex')],
+        }),
+        tailwind()
+    ],
 
 });
