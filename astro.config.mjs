60519df2c7ddced2082900c4ffe3c41a4cc75629
JorgeSlime <jt603691@gmail.com>
2025-02-15 17:15:14 -0400

    add latex into mdx doc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,35 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import expressiveCode, { astroExpressiveCode } from 'astro-expressive-code';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import remarkToc from 'remark-toc';
 
 export default defineConfig({
     site: 'https://jorgeslime.github.io',
     output: 'static',
     markdown: {
+        remarkPlugins: [remarkMath, remarkToc],
+        rehypePlugins: [rehypeKatex],
         //shikiConfig: {
-        //    theme: 'dracula',
+        //    theme: 'vitesse-black',
         //},
     },
     integrations: [
+    tailwind(), expressiveCode(),
         astroExpressiveCode({
             theme: ['vitesse-black'],
             shiki: {
-                langs: [],
+                langs: ['cpp'],
             }
         }),
         mdx({
-        //syntaxHighlight: 'shiki',
-        //shikiConfig: { theme: 'dracula' },
+            // remarkPlugins: [remarkMath],
+            // rehypePlugins: [rehypeKatex],
+        // syntaxHighlight: 'shiki',
+        // shikiConfig: { theme: 'vitesse-black' },
         }), 
-        tailwind(), expressiveCode()
+        
     ],
 
 });
--- ec.config.mjs
+++ ec.config.mjs
@@ -2,5 +2,6 @@ import { defineEcConfig } from 'astro-expressive-code'
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
 export default defineEcConfig({
+    themes: ['vitesse-black'],
     plugins: [pluginLineNumbers()],
 })
